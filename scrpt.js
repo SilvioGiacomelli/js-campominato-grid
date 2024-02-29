e1079033fc3ecfab92ffb6679e539a578d92d505
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-02-29 18:00:14 +0100

    push

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,80 @@
-//////////////////////////////////////
-//////////////////////////////////////
-//////////////////////////////////////
+const gridContainer = document.querySelector(".grid-container")
+const btnStart = document.querySelector(".btn-start");
+
+
+///Bottone start
+btnStart.addEventListener("click", inizio)
+
+
+///////////////////////////////////////////////
+////////////////Ciclo 100lementi///////////////
+///////////////////////////////////////////////
+
+function inizio(){
+  reset()
+
+  for(let i = 1; i <= 100; i++){
+  const square = inserisciRiquadro(i)
+
+  gridContainer.append(square)
+}
+}
+
+
+// Definizione della funzione inserisciRiquadro che crea un quadrato (div) con un numero associato
+function inserisciRiquadro(numero){
+  
+  // Crea un nuovo elemento div e lo assegna alla variabile 'sq'
+  const sq = document.createElement("div")
+
+  // Imposta la classe dell'elemento a "square"
+  sq.className = "square"; 
+
+  // Imposta una proprietà personalizzata '_sqID' con il valore del 'numero' passato alla funzione
+  sq._sqID = numero
+  
+
+
+  /////////////////////////////
+  //////Funzione///Click///////
+  /////////////////////////////
+  
+  sq.addEventListener("click", 
+  
+  function(){
+    
+    console.log(this.dataset.sqid)
+    
+    const numero = this._sqID;
+    
+    this.innerHTML = this._sqID;
+
+    // Se l'elemento non ha la classe 'clicked', mostra il numero all'interno dell'elemento
+
+    if (!this.classList.contains("clicked")) {
+      this.innerHTML = this._sqID;
+    } else {
+      this.innerHTML = "";
+    }
+    
+    this.classList.toggle("clicked");
+
+    // Alterna una classe basata sul fatto che il numero sia pari o dispari
+    // (si assume che la funzione getOddEvenClass esista e determini la classe corretta in base al numero)
+    this.classList.toggle(getOddEvenClass(numero));
+    
+  })
+  // Restituisce l'elemento 'sq' così che possa essere aggiunto al codice
+  return sq
+}
+
+
+///////////////////////
+/////////RESET/////////
+///////////////////////
+
+function reset(){
+
+  console.log(this)
+  gridContainer.innerHTML = ""; 
+}
